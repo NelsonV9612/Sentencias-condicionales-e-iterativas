5e9fb33104f6d198de157db550bbb30c08ed52ad
Nelson Valdes <nelson9612@hotmail.com>
2022-06-14 16:16:32 -0300

    Ej1

---
+++ src/Sentencias/mayoMenor.java
@@ -0,0 +1,21 @@
+package Sentencias;
+
+import java.util.Scanner;
+
+public class mayoMenor {
+    public static void main(String[] args) {
+        Scanner read= new Scanner(System.in);
+        double num1;
+        double num2;
+        System.out.println("--------------Bienvenido-------------");
+        System.out.println("Ingrese el primer numero a comparar: ");
+        num1=read.nextDouble();
+        System.out.println("Ingrese el segundo numero a comparar: ");
+        num2=read.nextDouble();
+        if (num1>num2){
+            System.out.println("El número "+num1+" es mayor que "+num2);
+        }else {
+            System.out.println("El número "+num2+" es mayor que "+num1);
+        }
+    }
+}
