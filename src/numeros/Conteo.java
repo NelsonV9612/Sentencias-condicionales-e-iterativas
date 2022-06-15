bd19d2e36110861cbc4860797f99cf616eeb8a1e
Nelson Valdes <nelson9612@hotmail.com>
2022-06-14 21:46:43 -0300

    ej2-conteo de numeros

---
+++ src/numeros/Conteo.java
@@ -0,0 +1,10 @@
+package numeros;
+
+public class Conteo {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
