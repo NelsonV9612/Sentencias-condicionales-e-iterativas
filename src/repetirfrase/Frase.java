2eec8e6f2a06d4fc9a2dd3b45e82863053691c16
Nelson Valdes <nelson9612@hotmail.com>
2022-06-14 21:26:12 -0300

    ej1 foo

---
+++ src/repetirfrase/Frase.java
@@ -0,0 +1,14 @@
+package repetirfrase;
+
+import java.util.Scanner;
+
+public class Frase {
+    public static void main(String[] args) {
+        String frase="foo";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(frase);
+            
+        }
+
+    }
+}
