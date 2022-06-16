19ba8a255a49539ab38d8701246865624ec61dce
Nelson Valdes <nelson9612@hotmail.com>
2022-06-16 19:45:11 -0300

    Ej3 triaungulos

---
+++ src/triangulo/TriangulosVarios.java
@@ -0,0 +1,37 @@
+package triangulo;
+
+import java.util.Scanner;
+
+public class TriangulosVarios {
+    public static void main(String[] args) {
+        Scanner read=new Scanner(System.in);
+        int alto;
+        System.out.println("----------------Bienvenido------------");
+        System.out.println("Ingrese el alto del triangulo: ");
+        alto=read.nextInt();
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j<=i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("-------------------------");
+        for (int i = 0; i < alto; i++) {
+            for (int j = alto-1-i; j >= 0; j--) {
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("--------------------");
+        for (int i = 0; i < alto; i++) {
+            for (int j = alto-1-i; j >=0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
